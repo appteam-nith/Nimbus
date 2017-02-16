bcc07bf493c99816a31aa2525bc5441be3641e33
octacode <allblue.shasha@gmail.com>
2017-02-17 00:41:20 +0530

    Upgraded the compile SDK version and added the basic layout of the MainActivity.

--- app/src/main/java/com/nith/appteam/nimbus/Activity/MainActivity.java
+++ app/src/main/java/com/nith/appteam/nimbus/Activity/MainActivity.java
@@ -1,10 +1,16 @@
 package com.nith.appteam.nimbus.Activity;
 
 import android.content.Intent;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.nith.appteam.nimbus.Adapter.MainRecyclerAdapter;
 import com.nith.appteam.nimbus.R;
 import com.nith.appteam.nimbus.Utils.SharedPref;
 
@@ -12,34 +18,65 @@ import com.nith.appteam.nimbus.Utils.SharedPref;
 public class MainActivity extends AppCompatActivity {
 
 
- private SharedPref sharedPref;
-
+    private SharedPref sharedPref;
+    private RecyclerView mRecyclerView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        sharedPref=new SharedPref(this);
-        if(!sharedPref.getSkipStatus()){
+        sharedPref = new SharedPref(this);
+        if (!sharedPref.getSkipStatus()) {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
             finish();
         }
         setContentView(R.layout.activity_main);
 
+        initCollapsingToolbar();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-
+        mRecyclerView = (RecyclerView) findViewById(R.id.main_recycler_view);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        mRecyclerView.setAdapter(new MainRecyclerAdapter());
     }
 
 
-    public void openProfile(View view){
+    public void openProfile(View view) {
 
-        startActivity(new Intent(this,ProfileActivity.class));
+        startActivity(new Intent(this, ProfileActivity.class));
 
     }
 
-    public void openTeam(View view){
-        startActivity(new Intent(this,TeamActivity.class));
+    public void openTeam(View view) {
+        startActivity(new Intent(this, TeamActivity.class));
     }
 
+    private void initCollapsingToolbar() {
+        final CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(" ");
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        appBarLayout.setExpanded(true);
 
+        // hiding & showing the title when toolbar expanded & collapsed
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbar.setTitle(getString(R.string.app_name));
+                    isShow = true;
+                } else if (isShow) {
+                    collapsingToolbar.setTitle(" ");
+                    isShow = false;
+                }
+            }
+        });
+    }
 }
