b000e57f5f3d805cdc559bdcd83cf8935294eab5
akatsuki06 <suraznegi@gmail.com>
2017-02-17 19:03:52 +0530

    teamevent act linked to its endpoint

--- app/src/main/java/com/nith/appteam/nimbus/TeamEventActivity.java
+++ app/src/main/java/com/nith/appteam/nimbus/TeamEventActivity.java
@@ -10,6 +10,7 @@ import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,15 +21,22 @@ import android.widget.Toast;
 
 import com.github.florent37.materialleanback.MaterialLeanBack;
 import com.nith.appteam.nimbus.Fragment.TeamFragment;
+import com.nith.appteam.nimbus.Model.TeamEventList;
+import com.nith.appteam.nimbus.Utils.Connection;
+import com.nith.appteam.nimbus.Utils.Util;
 
 import java.util.ArrayList;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class TeamEventActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener {
 
-    private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR  = 0.9f;
-    private static final float PERCENTAGE_TO_HIDE_TITLE_DETAILS     = 0.3f;
-    private static final int ALPHA_ANIMATIONS_DURATION              = 200;
-    private boolean mIsTheTitleVisible          = false;
+    private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR = 0.9f;
+    private static final float PERCENTAGE_TO_HIDE_TITLE_DETAILS = 0.3f;
+    private static final int ALPHA_ANIMATIONS_DURATION = 200;
+    private boolean mIsTheTitleVisible = false;
     private boolean mIsTheTitleContainerVisible = true;
     private LinearLayout mTitleContainer;
     private TextView mTitle;
@@ -39,19 +47,20 @@ public class TeamEventActivity extends AppCompatActivity implements AppBarLayout
     private String id; // Team id for the Extracting team detail,event and projects
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.EventAct);
         setContentView(R.layout.activity_teamevent);
         bindActivity();
-        Intent i=getIntent();
-        if(i!=null){
-            if(i.hasExtra(TeamFragment.TEAM_ID)){
-                id=i.getStringExtra(TeamFragment.TEAM_ID);
+        Intent i = getIntent();
+        if (i != null) {
+            if (i.hasExtra(TeamFragment.TEAM_ID)) {
+                id = i.getStringExtra(TeamFragment.TEAM_ID);
             }
         }
+
+        getRespose(id);
 
         mAppBarLayout.addOnOffsetChangedListener(this);
         startAlphaAnimation(mTitle, 0, View.INVISIBLE);
@@ -89,7 +98,7 @@ public class TeamEventActivity extends AppCompatActivity implements AppBarLayout
             @Override
             public void onBindViewHolder(EventViewHolder viewHolder, int i) {
                 viewHolder.textView.setText("Pic" + i);
-                viewHolder.imageView.setImageBitmap(images.get(i%6));
+                viewHolder.imageView.setImageBitmap(images.get(i % 6));
             }
 
             @Override
@@ -110,15 +119,16 @@ public class TeamEventActivity extends AppCompatActivity implements AppBarLayout
 
             @Override
             public boolean isCustomView(int row) {
-                return row==0;
+                return row == 0;
             }
+
             @Override
             public boolean hasRowTitle(int row) {
                 return row != 6;
             }
 
 
-//
+            //
             @Override
             public void onBindCustomView(RecyclerView.ViewHolder viewHolder, int row) {
                 super.onBindCustomView(viewHolder, row);
@@ -140,19 +150,18 @@ public class TeamEventActivity extends AppCompatActivity implements AppBarLayout
             }
         });
 
-
-
     }
 
     private void bindActivity() {
-        mToolbar        = (Toolbar) findViewById(R.id.main_toolbar);
-        mTitle          = (TextView) findViewById(R.id.main_textview_title);
+        mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        mTitle = (TextView) findViewById(R.id.main_textview_title);
         mTitleContainer = (LinearLayout) findViewById(R.id.main_linearlayout_title);
-        mAppBarLayout   = (AppBarLayout) findViewById(R.id.main_appbar);
+        mAppBarLayout = (AppBarLayout) findViewById(R.id.main_appbar);
         materialLeanBack = (MaterialLeanBack) findViewById(R.id.materialLeanBack);
     }
-    private void addImages(){
-        images=new ArrayList<>();
+
+    private void addImages() {
+        images = new ArrayList<>();
         images.add(BitmapFactory.decodeResource(getResources(), R.drawable.forest_2));
         images.add(BitmapFactory.decodeResource(getResources(), R.drawable.forest_1));
         images.add(BitmapFactory.decodeResource(getResources(), R.drawable.forest_3));
@@ -160,7 +169,6 @@ public class TeamEventActivity extends AppCompatActivity implements AppBarLayout
         images.add(BitmapFactory.decodeResource(getResources(), R.drawable.forest_5));
         images.add(BitmapFactory.decodeResource(getResources(), R.drawable.forest_6));
     }
-
 
     @Override
     public void onOffsetChanged(AppBarLayout appBarLayout, int offset) {
@@ -172,30 +180,29 @@ public class TeamEventActivity extends AppCompatActivity implements AppBarLayout
         handleToolbarTitleVisibility(percentage);
     }
 
+    private void handleToolbarTitleVisibility(float percentage) {
+        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
 
-   private void handleToolbarTitleVisibility(float percentage) {
-            if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
+            if (!mIsTheTitleVisible) {
+                startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                mIsTheTitleVisible = true;
 
-                if(!mIsTheTitleVisible) {
-                    startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
-                    mIsTheTitleVisible = true;
-
-                    mToolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-                }
-
-            } else {
-
-                if (mIsTheTitleVisible) {
-                    startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
-                    mIsTheTitleVisible = false;
-                    mToolbar.setBackgroundColor(Color.alpha(0));
-                }
+                mToolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
             }
+
+        } else {
+
+            if (mIsTheTitleVisible) {
+                startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
+                mIsTheTitleVisible = false;
+                mToolbar.setBackgroundColor(Color.alpha(0));
+            }
+        }
     }
 
     private void handleAlphaOnTitle(float percentage) {
         if (percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS) {
-            if(mIsTheTitleContainerVisible) {
+            if (mIsTheTitleContainerVisible) {
                 startAlphaAnimation(mTitleContainer, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
                 mIsTheTitleContainerVisible = false;
             }
@@ -209,13 +216,52 @@ public class TeamEventActivity extends AppCompatActivity implements AppBarLayout
         }
     }
 
-    public static void startAlphaAnimation (View v, long duration, int visibility) {
+    public static void startAlphaAnimation(View v, long duration, int visibility) {
         AlphaAnimation alphaAnimation = (visibility == View.VISIBLE)
-            ? new AlphaAnimation(0f, 1f)
-            : new AlphaAnimation(1f, 0f);
+                ? new AlphaAnimation(0f, 1f)
+                : new AlphaAnimation(1f, 0f);
 
         alphaAnimation.setDuration(duration);
         alphaAnimation.setFillAfter(true);
         v.startAnimation(alphaAnimation);
+    }
+
+
+
+
+    void getRespose(String id) {
+        // TODO: 14/2/17   add progress bar to the act
+
+        // progressBar.setVisibility(View.VISIBLE);
+        if (new Connection(this).isInternet()) {
+            Call<TeamEventList> call = Util.getRetrofitService().getTeamEvents(id);
+           call.enqueue(new Callback<TeamEventList>() {
+               @Override
+               public void onResponse(Call<TeamEventList> call, Response<TeamEventList> response) {
+
+                   if(!response.isSuccess()){Log.v("TESTTEAMEVENTRESPONSE","NOT Working!");return;}
+                   TeamEventList teamEvent=response.body();
+
+                   if(!teamEvent.isSucess()){Log.v("TESTTEAMEVENTRESPONSE","NOT Working!");return;}
+
+                 
+
+
+
+               }
+
+               @Override
+               public void onFailure(Call<TeamEventList> call, Throwable t) {
+                   Toast.makeText(getApplicationContext(), "Please check your network connection and internet permission", Toast.LENGTH_SHORT).show();
+                   //progressBar.setVisibility(View.GONE);
+               }
+           });
+
+        } else {
+//            message.setVisibility(View.VISIBLE);
+//            message.setText("Please Check Your Internet Connection");
+//            progressBar.setVisibility(View.GONE);
+        }
+
     }
 }
--- app/src/main/java/com/nith/appteam/nimbus/Utils/ApiInterface.java
+++ app/src/main/java/com/nith/appteam/nimbus/Utils/ApiInterface.java
@@ -1,9 +1,13 @@
 package com.nith.appteam.nimbus.Utils;
 
+import com.nith.appteam.nimbus.Model.TeamEventList;
 import com.nith.appteam.nimbus.Model.TeamListResponse;
 
 import retrofit2.Call;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by sahil on 9/2/17.
@@ -13,4 +17,10 @@ public interface ApiInterface {
 
     @GET("team")
     Call<TeamListResponse> getAllTeam();
+
+    @GET("team/{id}")
+    Call<TeamEventList> getTeamEvents(@Path("id") String id);
+
+
+
 }
