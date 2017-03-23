22db5276e17302ab77c24644e0b9527d66d70c52
octacode <allblue.shasha@gmail.com>
2017-03-23 19:23:22 +0530

    Updated the leaderboard.xml

--- app/src/main/java/com/nith/appteam/nimbus/Activity/SlidingImageDetailActivity.java
+++ app/src/main/java/com/nith/appteam/nimbus/Activity/SlidingImageDetailActivity.java
@@ -28,6 +28,7 @@ public class SlidingImageDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle("");
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sliding_image_detail);
