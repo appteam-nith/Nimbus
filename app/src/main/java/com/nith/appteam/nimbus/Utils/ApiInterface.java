585070938b77ec7ed9d8c8603ec8090a0bf44bf1
akatsuki06 <suraznegi@gmail.com>
2017-03-24 19:20:49 +0530

    merged conflicts

--- app/src/main/java/com/nith/appteam/nimbus/Utils/ApiInterface.java
+++ app/src/main/java/com/nith/appteam/nimbus/Utils/ApiInterface.java
@@ -39,77 +39,77 @@ import retrofit2.http.Query;
 
 public interface ApiInterface {
 
-    @GET("team_95")
+    @GET("team")
     Call<TeamListResponse> getAllTeam();
-    @GET("team_core_95")
+    @GET("team_core")
     Call<CoreTeamResponse> getAllCoreTeam();
 
 
-    @GET("workshop_95")
+    @GET("workshop")
     Call<WorkshopListResponse> getAllWorkshop();
 
-    @GET("workshop_95/{id}")
+    @GET("workshop/{id}")
     Call<SingleWorkshopResponse> getSingleWorkshop(@Path("id") String id);
 
-    @GET("event_95/{event_id}")
+    @GET("event/{event_id}")
     Call<SingleWorkshopResponse> getEventDetail(@Path("event_id") String event_id, @Query("student_id") String student_id);
 
 
-    @POST("event/register_95/{event_id}")
+    @POST("event/register/{event_id}")
     Call<EventRegisterResponse> getEventRegisterResponse(@Path("event_id") String event_id, @Query("student_id") String student_id);
 
-    @GET("team_95/{id}")
+    @GET("team/{id}")
     Call<TeamEventList> getTeamEvents(@Path("id") String id);
 
-    @GET("team_core_95/{id}")
+    @GET("team_core/{id}")
     Call<CoreTeamEvents> getCoreTeamEvents(@Path("id") String id);
 
-    @POST("register_95")
+    @POST("register")
     @FormUrlEncoded
     Call<FbLoginFragment.UserSentResponse> sendFbUserData(@Field("name") String name,@Field("email") String email,@Field("pic_url")String picUrl);
 
-    @GET("quiz/get_95/{id}")
+    @GET("quiz/get/{id}")
     Call<QuizQuestionsModel> getQuiz(@Path("id") String id);
 
-    @GET("quiz/update_95/{id}")
+    @GET("quiz/update/{id}")
     Call<UpdateScoreModel> updateScore(@Path("id") String id, @Query("score") int score);
 
     @FormUrlEncoded
-    @POST("newsfeed/post_95/{student_id}")
+    @POST("newsfeed/post/{student_id}")
     Call<UploadNewsFeedActivity.UploadResponse> uploadNews(@Field("title") String title, @Field("desc") String description, @Path("student_id") String userId, @Field("name") String userName,@Field("photo") String imageUrl);
 
 
-    @GET("newsfeed/getall_95/{id}")
+    @GET("newsfeed/getall/{id}")
     Call<NewsFeedResponse> getAllNews(@Path("id") String userId,@Query("from") int from);
 
 
-    @POST("newsfeed/like_95/{id}")
+    @POST("newsfeed/like/{id}")
     Call<Likecount>likecount(@Path("id") String id, @Query("student_id") String userId);
 
-    @GET("quiz/leaderboard_95")
+    @GET("quiz/leaderboard")
     Call<LeaderBoardModel> getLeaderBoard();
 
-    @GET("profile_95/{id}")
+    @GET("profile/{id}")
     Call<ProfileDataModel> profileBasicInfo(@Path("id") String id);
 
-    @GET("profile/event_95/{id}")
+    @GET("profile/event/{id}")
     Call<ProfileEventModel> profileEventList(@Path("id") String id);
 
-    @GET("profile/newsfeed_95/{student_id}")
+    @GET("profile/newsfeed/{student_id}")
     Call<NewsFeedResponse> getUserNews(@Path("student_id") String userId);
 
-    @GET("main/images/get_95")
+    @GET("main/images/get")
     Call<MainPagerResponse> getMainResponse();
 
-    @GET("gallery_95/{id}")
+    @GET("gallery/{id}")
     Call<GalleryDetailResponse> getGalleryResponse(@Path("id") String id);
 
-    @GET("galleryAll_95")
+    @GET("galleryAll")
     Call<GalleryResponse> getGalleryAll();
 
-    @POST("update/rollno_95/{id}")
+    @POST("update/rollno/{id}")
     Call<RegisterResponse> updateRollNo(@Path("id") String id,@Query("roll_no") String rollNo);
 
-    @GET("sponsor_95")
+    @GET("sponsor")
     Call<SponsorResponse> getSponsorList();
 }
